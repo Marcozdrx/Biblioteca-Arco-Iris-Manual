89decc10ec2d73e86354d4f657014392571f2485
Marcos Paulo Fernandes <gameeerluk@gmail.com>
2025-09-02 16:16:51 -0300

    Deletar livro
    Feito arquivo para deletar os livros do banco de dados, tambem os fazendo desaparecer das paginas de admin e usuario

--- PHP/DeletarLivros.php
+++ PHP/DeletarLivros.php
@@ -1,3 +1,19 @@
 <?php
+require_once 'conexao.php';
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $id = $_POST['id'];
+    $sqlDeletarLivro = "DELETE FROM livros WHERE id = :id";
+    $stmt = $pdo->prepare($sqlDeletarLivro);
+    $stmt->bindParam(':id', $id);
+    
+    if($stmt->execute()){
+        echo "<script>alert('Livro deletado com sucesso')</script>";
+        header("Location: ../HTML/inicio-admin.php");
+    }else{
+        echo "<script>alert('Erro ao deletar livro')</script>";
+    }
+}
 
 ?>
