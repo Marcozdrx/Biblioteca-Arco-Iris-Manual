e0cbb93550ed6423847d9b23afab0a8276bae826
Marcos Paulo Fernandes <gameeerluk@gmail.com>
2025-09-02 23:23:48 -0300

    Editar livros
    Feito o arquivo que processa a atualização dos livros, puxando os dados no formularios e protegendo eles quando forem enviados para o banco de dados

--- PHP/editarLivros.php
+++ PHP/editarLivros.php
@@ -4,28 +4,55 @@ session_start();
 
 
 
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $id = $_POST['id'];
-    $sqlDeletarLivro = "UPDATE livros SET ativo = FALSE WHERE id = :id";
-    $stmt = $pdo->prepare($sqlDeletarLivro);
+    $id = $_POST['idLivroEdit'];
+    $titulo = $_POST['tituloEdit'];
+    $estoque = $_POST['estoqueEdit'];
+    $autor_id = $_POST['autorEdit'];
+    $imagem_capa = $_FILES['capaEdit'];
+    $dataPublicacao = $_POST['dataPublicacaoEdit'];
+    $numero_paginas = $_POST['numeroPaginasEdit'];
+    $editora = $_POST['editoraEdit'];
+    $isbn = $_POST['isbnEdit'];
+    $idioma = $_POST['idiomaEdit'];
+    $categoria_id = $_POST['categoriaEdit'];
+    $descricao = $_POST['descricaoEdit'];
+    
+    $sqlAttLivro = "UPDATE livros SET 
+    titulo = :titulo, 
+    autor_id = :autor, 
+    categoria_id = :categoria, 
+    isbn = :isbn, 
+    ano_publicacao = :ano_publicacao, 
+    numero_paginas = :numero_paginas, 
+    descricao = :descricao, 
+    estoque = :estoque, 
+    editora = :editora, 
+    idioma = :idioma,
+    imagem_capa = :imagem_capa
+    WHERE id = :id";
+    
+    
+    $stmt = $pdo->prepare($sqlAttLivro);
     $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':titulo', $titulo);
+    $stmt->bindParam(':imagem_capa', $imagem_capa, PDO::PARAM_LOB);
+    $stmt->bindParam(':estoque', $estoque);
+    $stmt->bindParam(':autor', $autor_id, PDO::PARAM_INT);
+    $stmt->bindParam(':ano_publicacao', $dataPublicacao); 
+    $stmt->bindParam(':numero_paginas', $numero_paginas);
+    $stmt->bindParam(':editora', $editora);
+    $stmt->bindParam(':isbn', $isbn);
+    $stmt->bindParam(':idioma', $idioma);
+    $stmt->bindParam(':categoria', $categoria_id);
+    $stmt->bindParam(':descricao', $descricao);
     
     if($stmt->execute()){
-        echo "<script>alert('Livro deletado com sucesso')</script>";
+        echo "<script>alert('Livro atualizado com sucesso')</script>";
         header("Location: ../HTML/inicio-admin.php");
     }else{
-        echo "<script>alert('Erro ao deletar livro')</script>";
+        echo "<script>alert('Erro ao atualizar livro')</script>";
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-</body>
-</html>
